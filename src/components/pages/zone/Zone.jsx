b3dda4dbd2f879135c9a75a1722afa836c46da4c
Mark Tobler <marktobler@gmail.com>
2022-03-03 17:49:58 -0800

    feat/ Zone GPM, GPM Lifetime implemented

--- src/components/pages/zone/Zone.jsx
+++ src/components/pages/zone/Zone.jsx
@@ -1,64 +1,139 @@
 import "./zone.css";
-import ZoneFeaturedInfo from '../../zoneFeaturedInfo/ZoneFeaturedInfo'
-import { CropOriginal, Check, Opacity, SquareFoot, Landscape, LocalFlorist, WbSunny } from "@material-ui/icons";
-import { usageData } from '../../../dummy'
-import Chart from '../../chart/Chart'
+import ZoneFeaturedInfo from "../../zoneFeaturedInfo/ZoneFeaturedInfo";
+import {
+  CropOriginal,
+  Check,
+  Opacity,
+  SquareFoot,
+  Landscape,
+  LocalFlorist,
+  WbSunny,
+  DateRange,
+  Watch
+} from "@material-ui/icons";
+import moment from "moment";
+import { usageData } from "../../../dummy";
+import Chart from "../../chart/Chart";
+import React from "react";
+import axios from "axios";
 
-export default function Zone({id}) {
-  return (
-    <div className="zone">
-      <div className="zoneTitleContainer">
-        <h1 className="zoneTitle">Zone {id} Detail</h1>
-        <button className="zoneActionButton">action</button>
-      </div>
-      <div className="zoneContainer">
-        <div className="zoneShow">
-          <div className="zoneShowTop">
-            <img
-              src="https://prod-media-photo.rach.io/1d6dd916-e2d2-42c7-967c-f66e661daaca"
-              className="zoneShowImage"
-              alt=""
-            />
-            <div className="zoneShowTopTitle">
-              <span className="zoneShowName">Zone {id}</span>
-              <span className="zoneShowDesc">
-                Back planter, under stairs, and around BBQ
+export default class Zone extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { zone: {}, customNozzle: {}, customCrop: {}, customSoil: {}, customShade: {} };
+    this.id = this.props.id;
+  }
+  
+
+  componentDidMount() {
+    // axios.get(`https://jsonplaceholder.typicode.com/users`)
+    console.log("DEBUG ID: ", this.id);
+    axios
+      .get(`http://localhost:3030/api/v1/rachio/zone/${this.id}`)
+      .then((res) => {
+        const zone = res.data;
+        const customNozzle = zone['customNozzle'];
+        const customCrop = zone['customCrop'];
+        const customSoil = zone['customSoil'];
+        const customShade = zone['customShade'];
+
+        console.log(zone);
+        console.log('DEBUG:', customNozzle);
+
+        this.setState({ zone });
+        this.setState({ customNozzle })
+        this.setState({ customCrop })
+        this.setState({ customSoil })
+        this.setState({ customShade })
+      });
+  }
+
+  render() {
+    console.log("DEBUG: state:", this.state);
+    return (
+      <div className="zone">
+        <div className="zoneTitleContainer">
+          <h1 className="zoneTitle">{this.state.zone.name}</h1>
+          <button className="zoneActionButton">{this.state.zone.enabled ? "disable" : "enable"}</button>
+        </div>
+        <div className="zoneContainer">
+          <div className="zoneShow">
+            <div className="zoneShowTop">
+              <img
+                src={this.state.zone.imageUrl}
+                className="zoneShowImage"
+                alt=""
+              />
+            </div>
+            <div className="zoneShowBottom">
+              <span className="zoneShowTitle">
+                Zone {this.state.zone.zoneNumber} Details
               </span>
+              <div className="zoneShowBottomContainer">
+                <div className="zoneShowBottomCol">
+                  <div className="zoneShowInfo">
+                    <Check className={this.state.zone.enabled ? 'zoneShowIcon enabled' : 'zoneShowIcon disabled'} />
+                    <span className={this.state.zone.enabled ? 'zoneShowInfoTitle enabled' : 'zoneShowInfoTitle disabled'} >                      
+                      {this.state.zone.enabled ? "Enabled" : "Disabled"}
+                    </span>
+                  </div>
+                  <div className="zoneShowInfo">
+                    <Opacity className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle capitalize">{
+                     `${this.state.customNozzle.name}`.replace(/_/g, ' ')}</span>
+                  </div>
+                  <div className="zoneShowInfo">
+                    <SquareFoot className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle"> { this.state.customNozzle.inchesPerHour} inch/hour</span>
+                  </div>
+                </div>
+                <div className="zoneShowBottomCol">
+                  <div className="zoneShowInfo">
+                    <Landscape className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle capitalize">{`${this.state.customSoil.name}`.replace(/_/g, ' ')}</span>
+                  </div>
+                  <div className="zoneShowInfo">
+                    <LocalFlorist className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle capitalize">{this.state.customCrop.name}</span>
+                  </div>
+                  <div className="zoneShowInfo">
+                    <WbSunny className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle capitalize">{`${this.state.customShade.name}`.replace(/_/g, ' ')}</span>
+                  </div>
+                </div>
+                </div>              
             </div>
-          </div>          
-          <div className="zoneShowBottom">
-            <span className="zoneShowTitle">Zone Details</span>
-            <div className="zoneShowInfo">
-              <Check className="zoneShowIcon" />
-              <span className="zoneShowInfoTitle">Enabled</span>
-            </div>
-            <div className="zoneShowInfo">
-              <Opacity className="zoneShowIcon" />
-              <span className="zoneShowInfoTitle">Bubbler</span>
-            </div>
-            <div className="zoneShowInfo">
-              <SquareFoot className="zoneShowIcon" />
-              <span className="zoneShowInfoTitle">1 inch/hour</span>
-            </div>
-            <div className="zoneShowInfo">
-              <Landscape className="zoneShowIcon" />
-              <span className="zoneShowInfoTitle">Loamy Sand</span>
-            </div>
-            <div className="zoneShowInfo">
-              <LocalFlorist className="zoneShowIcon" />
-              <span className="zoneShowInfoTitle">Shrubs</span>
-            </div>
-            <div className="zoneShowInfo">
-              <WbSunny className="zoneShowIcon" />
-              <span className="zoneShowInfoTitle">Lots of Sun</span>
+            <div className="zoneShowFooter">
+              <span className="zoneShowFooterTitle">Last Watered:</span>
+              <div className="zoneShowBottomContainer">
+              <div className="zoneShowBottomCol">
+                  <div className="zoneShowInfo">
+                    <DateRange className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle">{moment(new Date(this.state.zone.lastWateredDate)).format("MMM d, YYYY")}</span>
+                  </div>
+                </div>
+                <div className="zoneShowBottomCol">
+                  <div className={`zoneShowInfo ${this.state.zone.lastWateredDuration ? "" : "hide"}`}>
+                    <Watch className="zoneShowIcon" />
+                    <span className="zoneShowInfoTitle">{Math.round(this.state.zone.lastWateredDuration /60)} mins</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="zoneAnalytics">
-        <ZoneFeaturedInfo />
-        <Chart data={usageData} title='Water Usage' grid dataKey='Monthly Usage' className='chart'/>
+          <div className="zoneAnalytics">
+            <ZoneFeaturedInfo id={ this.props.id }/>
+            <Chart
+              data={usageData}
+              title="Water Usage"
+              grid
+              dataKey="Monthly Usage"
+              className="chart"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/zoneFeaturedInfo/ZoneFeaturedInfo.jsx
+++ src/components/zoneFeaturedInfo/ZoneFeaturedInfo.jsx
@@ -1,25 +1,72 @@
-import './zoneFeaturedInfo.css'
-import {ArrowDownward, ArrowUpward} from '@material-ui/icons'
+import "./zoneFeaturedInfo.css";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import React from "react";
+import axios from "axios";
 
-export default function ZoneFeaturedInfo() {
-  return (
-     <div className='featured'>
-        <div className='featuredItem'>
-           <span className="featuredTitle">Average GPM</span>
-           <div className="featuredMoneyContainer">
-              <span className="featuredMoney">23.5</span>
-              <span className="featuredMoneyRate">-11.5<ArrowDownward className='featuredIcon negative' /></span>              
-           </div>
-           <span className="featuredSub">Last Full Cycle</span>
+export default class ZoneFeaturedInfo extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { gpmTotal: {}, totalDuration: {}, gpmCurrent: {}, gpmDuration: {} };
+    this.id = this.props.id;
+  }  
+
+  componentDidMount() {
+    // axios.get(`https://jsonplaceholder.typicode.com/users`)
+    console.log("ZFI DEBUG ID: ", this.id);
+    axios
+      .get(`http://localhost:3030/api/v1/usage/gpm/${this.id}`)
+      .then((res) => {
+        const gpmTotal = res.data[0];
+        const totalDuration = gpmTotal.duration;        
+        this.setState({ gpmTotal });
+        this.setState({ totalDuration });
+        // console.log('ZFI DEBUG:', this.state);
+      });
+      
+    axios
+      .get(`http://localhost:3030/api/v1/usage/gpm/${this.id}/current`)
+      .then((res) => {
+        const gpmCurrent = res.data[0];
+        const currentDuration = gpmCurrent.duration;        
+        this.setState({ gpmCurrent });
+        this.setState({ currentDuration });
+        console.log('ZFI DEBUG:', this.state);
+      });
+  }
+
+  render() {
+    let arrow;
+    if (this.state.gpmTotal.gpm > this.state.gpmCurrent.gpm) {
+      arrow = <ArrowDownward className="featuredIcon" />
+    } else {
+      arrow = <ArrowUpward className="featuredIcon warning" />
+    }
+
+    return (
+      <div className="featured">
+        <div className="featuredItem">
+          <span className="featuredTitle">Current GPM</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">{parseFloat(this.state.gpmCurrent.gpm).toFixed(2)}</span>
+            <span className="featuredMoneyRate">
+              { parseFloat((1 - (this.state.gpmTotal.gpm/this.state.gpmCurrent.gpm)) * 100).toFixed(1) + '%'}
+              {arrow}
+            </span>
+          </div>
+          <span className="featuredSub">Last Full Cycle</span>
         </div>
-        <div className='featuredItem'>
-           <span className="featuredTitle">Lifetime GPM</span>
-           <div className="featuredMoneyContainer">
-              <span className="featuredMoney">28.8</span>
-              <span className="featuredMoneyRate"> +0.5 <ArrowUpward className='featuredIcon' /></span>              
-           </div>
-           <span className="featuredSub">Total hours: 1032</span>
+        <div className="featuredItem">
+          <span className="featuredTitle">Lifetime GPM</span>
+          <div className="featuredMoneyContainer">
+            <span className="featuredMoney">{parseFloat(this.state.gpmTotal.gpm).toFixed(2)}</span>
+            <span className="featuredMoneyRate">
+              {" "}              
+            </span>
+          </div>
+          <span className="featuredSub">Total time: {this.state.totalDuration.hours}:{this.state.totalDuration.minutes}</span>
         </div>
       </div>
-  )
+    );
+  }
 }
